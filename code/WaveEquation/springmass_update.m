e201db46dac188de0f37081cf5172f4d2cd65b77
hlee234 <84032135+hlee234@users.noreply.github.com>
2021-06-17 23:54:00 +0900

    Add files via upload

---
+++ code/WaveEquation/springmass_update.m
@@ -0,0 +1,50 @@
+%This code demostrates a wave equation with spring-mass system
+%using ode45
+%with length L
+%Solution to the PDE is uploaded in OneDrive. 
+
+
+%contains error
+
+
+
+add_my_paths;
+L=pi;  %length
+tt=10; %time
+K=3; %"N" value for fourier sine series
+NN   = 101;  dx = L/(NN-1); %infinitasimal x
+dt = tt/(NN-1); %infinitasimal t
+x   = 0:dx:L;%domain
+tspan   = 0:dt:tt; %time
+[X,T]=meshgrid(x,tspan); %generate matrix
+
+
+index=1;
+fig=figure;
+
+v=0; %u=v+w
+for nn=1:K
+    eigenf=sin(nn*pi*X/L); %eigenfunction
+    w = sin(T) + (X/pi)* -1 * sin(T);
+    %solve 2nd order diffeq
+    if nn==1
+        ic=[1,-2/pi]; %IC when n=1
+    else
+        ic=[(-2*sin(pi*nn))/(pi*(nn^2-1)),2*((nn^2-1)*sin(pi*nn)+(pi*nn^3*cos(pi*nn)+pi*nn))/(pi^2*nn^2*(nn^2-1))];
+    end
+    [t,a] = ode45(@(t,a) waveCoeff(t,a,nn), tspan, ic); %solve for a, the coefficient for Fourier Sine Series
+    v=v+a(:,1)'.*eigenf;
+end
+u=v+w; %final solution
+for i=1:NN
+    plot(x,u(i,:),'b-','LineWidth',2); 
+    xlabel('x'); ylabel('u'); 
+    set_positionFontsAll;
+    drawnow;
+    frame = getframe(fig);
+    im{index} = frame2im(frame);
+    index=index+1;
+    clf;
+end
+filename = sprintf('springmass.gif');
+im_to_gif(filename,im,index-1); %make .gif file for each n value
