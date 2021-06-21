4b45644565c2a1065b0426d0c2c5bcaa97c5ab00
hlee234 <84032135+hlee234@users.noreply.github.com>
2021-06-21 23:48:13 +0900

    Update plotFS_Gibbs.m

--- code/GibbsPhenomenon/plotFS_Gibbs.m
+++ code/GibbsPhenomenon/plotFS_Gibbs.m
@@ -1,10 +1,9 @@
-
 function plotFS_Gibbs(x,f,SinSeries,n,xrange)
 % plot the Gibbs phenomenon
-plot(x,f,'k','LineWidth',2);          hold on; 
-plot(x,SinSeries,'b-','LineWidth',2);
-yline( (max(f)-min(f))*0.09 + max(f)); 
+plot(x,f,'w','LineWidth',2);          hold on; 
+plot(x,SinSeries,'y-','LineWidth',2);
+yline( (max(f)-min(f))*0.09 + max(f), 'w','LineWidth',1); 
 xlim(xrange); 
-legend('Function f','Fourier Series','9% overshot line','location','best')
-title(sprintf('Gibbs Phenomenon (n=%i)',n) );
+legend('\color{white} Function f','\color{white} Fourier Series','\color{white} 9% overshot line','Color',[0 0 0],'location','best','EdgeColor',[1 1 1])
+title(sprintf('Gibbs Phenomenon (N=%i)',n) );
 end
