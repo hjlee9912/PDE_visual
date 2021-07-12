ecfbaed2c22ff1e018011c9b818af007b7d76b13
hlee234 <84032135+hlee234@users.noreply.github.com>
2021-07-12 23:48:57 +0900

    Add files via upload

---
+++ code/SLEP/vibrating_nonuniformString.m
@@ -0,0 +1,72 @@
+%heat flow in nonuniform rod without sources at x=[a,b]
+% rho*du"/d"t = T0*du"/d"x
+%assume Dirichlet boundary condition u(a,t) = 0 = u(b,t)
+%with IC u(x,0) = f(x); and du/dt(x,0) = g(x);
+
+%problem to fix: showing non-physical graph
+
+add_my_path;
+a  = 0;  b = pi;   % domain [a,b]
+N  = 99;           % number of x points excluding both ends
+dx = (b-a)/(N+1);  % "infitasimal" x
+x  = a:dx:b;       % domain
+f  = @(x) sin(x);
+g  = @(x) cos(x);
+
+rho = @(x) sin(x)+3; %density
+T0  = 5; %tension (constant)
+
+%set variables for SLEP.m
+P = x*0+T0;
+dPdx = x*0;
+Q = x*0;
+R = rho(x);
+%solve eigenvalue problem: 
+%T0d"+lambda*rho(x)*y=0 where y is eigenfunction
+[v,e]    = SLEP(N,x,P,dPdx,Q,R,'Dirichlet'); % solve SLEP, vector and eigenvaluse (ascending)
+fig=figure;
+plot(x',v(:,1:3),'linewidth',2)%show first three eigenfunction
+legend('\phi_1', '\phi_2','\phi_3','location','best')
+orthogonal = zeros(length(v(1,:)),length(v(1,:))); %show orthogonality
+for i=1:length(v(1,:))
+    for j=i:length(v(1,:))
+       orthogonal(i,j) = sum(v(:,i).*v(:,j).*R')*dx;
+       orthogonal(j,i) = orthogonal(i,j); 
+    end
+end
+figure;
+image(orthogonal,'CDataMapping','scaled')
+c=colorbar;
+
+%% now combine spatial and time parts
+%solution is 
+%u = sum(an*sin(lambda^(0.5))*t*y)+sum(bn*cos(lambda^(0.5))*t*y)
+fx = f(x); gx = g(x);
+an = zeros(1,N); bn = an; %solve coefficients  using orthogonality
+for n=1:N
+    an(n)=sum(gx.*v(:,n).*R)/(e(n)^(0.5)*sum(v(:,n).^2.*R));
+    bn(n)=sum(fx.*v(:,n).*R)/(sum(v(:,n).^2.*R));
+end
+
+%evaluate u and create animation
+index=1;
+fig=figure;
+K = 40; %number of iteration
+tt = 10; %time
+u = 0; %displacement
+for t = 0:0.5:tt
+    for n = 1:K
+        u = u+an(n)*sin(e(n)^(0.5))*t*v(:,n)+bn(n)*cos(e(n)^(0.5))*t*v(:,n);
+    end
+    plot(x,u,'linewidth',2)
+    ylim([-50 50])
+    xlabel('x')
+    ylabel('u')
+    drawnow; 
+    frame = getframe(fig);
+    im{index} = frame2im(frame);
+    index=index+1;
+    clf;
+end
+filename = 'nonuniformString.gif';
+im_to_gif(filename,im,index-1);
